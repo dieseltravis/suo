1246a512246fd09cd028a909880b03d2ce595b6a
travis <travis.hardiman@gmail.com>
2014-03-24 15:45:27 -0700

    Create app.js
    initial creation

---
+++ app.js
@@ -0,0 +1,9 @@
+/*!
+ * suo v0.0.1
+ * suō Latin /ˈsu.oː/ Verb 1. I sew, stitch. 2. I join, fasten together. 3 (figuratively) I devise, cobble. ( http://en.wiktionary.org/wiki/suo#Verb_4 )
+ *
+ * Copyright 2014 Travis Hardiman and other contributors
+ * Released under the MIT license
+ *
+ * Date: 2014-03-24
+ */
