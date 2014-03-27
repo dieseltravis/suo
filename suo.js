b7b45836b3c3541b743f76cb54ff3c33ab725d66
travis <travis.hardiman@gmail.com>
2014-03-27 15:59:31 -0400

    Update and rename app.js to suo.js
    header update

---
+++ suo.js
@@ -0,0 +1,13 @@
+/*!
+ * suo v0.0.1
+ * "suō Latin /ˈsu.oː/ Verb 1. I sew, stitch. 2. I join, fasten together. 3 (figuratively) I devise, cobble."
+ * http://en.wiktionary.org/wiki/suo#Verb_4
+ *
+ * The suo name is copyright 2014 Travis Hardiman, all rights reserved
+ *
+ * All code is owned by contributors
+ * Released under the MIT license
+ * http://opensource.org/licenses/MIT
+ * 
+ * Date: 2014-03-25
+ */
