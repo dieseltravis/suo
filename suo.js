3bae4145c34e6c5b1550650c9cb690e74dc031d7
travis <travis.hardiman@gmail.com>
2014-03-28 20:55:38 -0400

    Update suo.js

--- suo.js
+++ suo.js
@@ -3,8 +3,6 @@
  * "suō Latin /ˈsu.oː/ Verb 1. I sew, stitch. 2. I join, fasten together. 3 (figuratively) I devise, cobble."
  * http://en.wiktionary.org/wiki/suo#Verb_4
  *
- * The suo name is copyright 2014 Travis Hardiman, all rights reserved
- *
  * All code is owned by contributors
  * Released under the MIT license
  * http://opensource.org/licenses/MIT
