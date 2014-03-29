4af2844f628acbb92e9e00aa56fbb439f37c9337
travis <travis.hardiman@gmail.com>
2014-03-28 21:43:17 -0400

    Update suo.js

--- suo.js
+++ suo.js
@@ -1,11 +1,12 @@
 /*!
  * suo v0.0.1
- * "suō Latin /ˈsu.oː/ Verb 1. I sew, stitch. 2. I join, fasten together. 3 (figuratively) I devise, cobble."
- * http://en.wiktionary.org/wiki/suo#Verb_4
  *
- * All code is owned by contributors
- * Released under the MIT license
- * https://github.com/dieseltravis/suo/blob/master/LICENSE
+ * All code is owned by contributors: https://github.com/dieseltravis/suo/graphs/contributors
+ * Released under the MIT license: https://github.com/dieseltravis/suo/blob/master/LICENSE
  * 
- * Date: 2014-03-25
+ * Date: 2014-03-28
  */
+;(function (root) {
+  'use strict';
+  //TODO: paint bikeshed, but first...
+}(this));
